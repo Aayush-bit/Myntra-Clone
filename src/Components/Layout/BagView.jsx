a8abfb5bbdbe7770630355ba208657b47be895c7
Aayush Mahajan <mahajanaayush23@gmail.com>
2022-04-27 19:18:14 +0530

    added comments

--- src/Components/Layout/BagView.jsx
+++ src/Components/Layout/BagView.jsx
@@ -1,42 +1,56 @@
+// this is a togglable bag pop up 
+// appears when user clicks the bag icon on the navbar
+// disappears when user clicks the "BACK" button on the BagView component
+
 import React from 'react'
-import { toggleBagView,addItemToBag,removeItemFromBag } from '../../redux/Products'
-import { useSelector,useDispatch } from 'react-redux'
+
+
+import { toggleBagView, addItemToBag, removeItemFromBag } from '../../redux/Products'
+import { useSelector, useDispatch } from 'react-redux'
+
+
 import './BagView.css'
 
+
+
 const BagView = () => {
+    // getting items present in the bag
     const data = useSelector((state) => state.product.current.bag)
     const dispatch = useDispatch()
+
     return (
         <div id="screen-overlay">
             <div>
-                <h3>your selected items</h3>
+                <h3>your bag</h3>
                 <div id="bag-items">
-                {
-                    data.map((el) => {
-                        return(
-                            <div className="bag-items-info" key={el.item.productId}>
-                                <img width="40%" src={el.item.searchImage} alt="product" />
-                                <div>
-                                    <h3>{el.item.brand}</h3>
-                                    <p>{el.item.additionalInfo}</p>
-                                    <div >
-                                        <p>{`Rs. ${el.item.price}`}</p>
-                                        <span>{`Rs. ${el.item.mrp}`}</span>
-                                        <span>{`${el.item.discountDisplayLabel}`}</span>
-                                    </div>
-                                    <p style={{ marginTop: "0.25rem" }}>{`Size: ${el.size}`}</p>
-                                    <p style={{ marginTop: "0.25rem" }}>{`Qty: ${el.count}`}</p>
-                                    <div className="bag-actions">
-                                        <button onClick={() => dispatch(addItemToBag({item: el.item, size: el.size}))}>Add</button>
-                                        <button onClick={() => dispatch(removeItemFromBag({ item: el.item, size: el.size }))}>Remove</button>
+                    {
+                        data.map((el) => {
+                            return (
+                                <div className="bag-items-info" key={el.item.productId}>
+                                    <img width="40%" src={el.item.searchImage} alt="product" />
+                                    <div>
+                                        <h3>{el.item.brand}</h3>
+                                        <p>{el.item.additionalInfo}</p>
+                                        <div >
+                                            <p>{`Rs. ${el.item.price}`}</p>
+                                            <span>{`Rs. ${el.item.mrp}`}</span>
+                                            <span>{`${el.item.discountDisplayLabel}`}</span>
+                                        </div>
+
+                                        <p style={{ marginTop: "0.25rem" }}>{`Size: ${el.size}`}</p>
+                                        <p style={{ marginTop: "0.25rem" }}>{`Qty: ${el.count}`}</p>
+
+                                        <div className="bag-actions">
+                                            <button onClick={() => dispatch(addItemToBag({ item: el.item, size: el.size }))}>Add</button>
+                                            <button onClick={() => dispatch(removeItemFromBag({ item: el.item, size: el.size }))}>Remove</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
                 </div>
-                <button onClick={() => dispatch(toggleBagView(false))}>close</button>
+                <button onClick={() => dispatch(toggleBagView(false))}>back</button>
             </div>
         </div>
     )
