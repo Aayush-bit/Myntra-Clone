cc4da0d1cc2d1c29983426928c36a8d3c3cfe987
Aayush Mahajan <mahajanaayush23@gmail.com>
2022-04-27 19:18:42 +0530

    added comments and made the code DRY

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,20 +1,33 @@
+// This is the common layout of different pages used in order to make the code DRY
+
 import React, { useEffect, useState } from "react";
+
+// redux
 import { useSelector, useDispatch } from "react-redux";
 import { searchProducts, toggleBagView } from "../../redux/Products";
+
 import BagView from "./BagView";
+
+// importing assets
 import Logo from "../../assets/images/logo.jpg";
-// import SearchIcon from "../../assets/images/search-icon.svg";
+// import SearchIcon from "../../assets/images/search-icon.svg";  // ! search icon not woring
 import ProfileIcon from "../../assets/images/profile-icon.svg";
 import HeartIcon from "../../assets/images/heart-icon.svg";
 import BagIcon from "../../assets/images/bag-icon.svg";
 import "./Layout.css";
 
+
+
 const Layout = ({ children, horizontalPadding }) => {
   const [searchValue, setSearchValue] = useState("")
   const dispatch = useDispatch()
+  const listOfTabs = ["Men", "Women", "Kids", "Home N' Living", "Beauty"]
+
+  // performing search action everytime the searchValue parameter is changed by the user
   useEffect(() => {
     dispatch(searchProducts(searchValue))
   }, [searchValue])
+
   const showBag = useSelector((state) => state.product.current.showBagView)
   return (
     <div
@@ -26,36 +39,23 @@ const Layout = ({ children, horizontalPadding }) => {
           <img width="53px" src={Logo} alt="myntra logo" />
         </div>
         <div className="tabs">
-          <div>
-            <a className="tab" href="/">
-              Men
-            </a>
-          </div>
-          <div>
-            <a className="tab" href="/">
-              Women
-            </a>
-          </div>
-          <div>
-            <a className="tab" href="/">
-              Kids
-            </a>
-          </div>
-          <div>
-            <a className="tab" href="/">
-              Home N' Living
-            </a>
-          </div>
-          <div>
-            <a className="tab" href="/">
-              Beauty
-            </a>
-          </div>
+          {
+            listOfTabs.map(tab => (
+              <div>
+                <a className="tab" href="/">
+                  {tab}
+                </a>
+              </div>
+            )
+            )
+          }
         </div>
         <div id="search-bar">
           {/* <div>
             <img width="15" src={SearchIcon} alt="search" />
           </div> */}
+
+          {/* Search bar in order to search required shirts */}
           <input
             type="text"
             placeholder="Search for products, brands and more"
